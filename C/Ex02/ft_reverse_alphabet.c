ee8efdf2cd3abbfa29ad9c72996d07c8980a33fd
Vincent Badalamenti <vincent.badalamenti08@gmail.com>
2025-04-30 18:58:20 +0200

    Update ft_reverse_alphabet.c

--- C/Ex02/ft_reverse_alphabet.c
+++ C/Ex02/ft_reverse_alphabet.c
@@ -12,14 +12,20 @@
 
 #include <unistd.h>
 
-void	ft_print_reverse_alphabet.	c(void)
+void	ft_print_reverse_alphabet(void)
 {
-	char	c;
+	char c;
 
 	c = 'z';
-	while (c <= 'a')
+	while (c >= 'a')
 	{
 		write(1, &c, 1);
-		c++;
+		c--;
 	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
 }
